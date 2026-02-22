09170e7445d9f0da855a18c2ddf472a11dfc7d24
Rahin Haque <haquerahin743@gmail.com>
2026-02-22 11:19:08 +0600

    Total cards count number added to header functions

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-console.log("Maga ladle");
+let totalCount = document.getElementById("total");
+let ThriveCount = document.getElementById("Thrive-count");
+let StrugglingCount = document.getElementById("Struggling-count");
+
+const allCardsSection = document.getElementById("allcards");
+console.log(allCardsSection.children.length);
+
+function calculateTotal(){
+  totalCount.innerText = allCardsSection.children.length;
+}
+calculateTotal();
+
